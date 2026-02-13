1fb251d239f8bfa9e8e898b6ab60983decd42060
sanjaych9517 <sanjaych9517@gmail.com>
2026-02-13 15:26:47 +0530

    Update avgOf3Numbers.java

--- Day_03/avgOf3Numbers.java
+++ Day_03/avgOf3Numbers.java
@@ -1,5 +1,17 @@
 package Day_03;
-
+import java.util.*;
 public class avgOf3Numbers {
-    
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter first number : ");
+        int num1 = sc.nextInt();
+
+        System.out.print("enter Second number :");
+        int num2 = sc.nextInt();
+
+        System.out.print("enter Third number : ");
+        int num3 = sc.nextInt();
+
+        int avg = (num1 + num2 +num3)/3;
+        System.out.println("Avg of three number = "  + avg);
 }

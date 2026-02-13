deae8ec713a23051de0f40f0b09e8b63d14ee797
sanjaych9517 <sanjaych9517@gmail.com>
2026-02-13 15:21:03 +0530

    Update areaOfSquare.java

--- Day_03/areaOfSquare.java
+++ Day_03/areaOfSquare.java
@@ -1,5 +1,11 @@
 package Day_03;
-
+import java.util.*;
 public class areaOfSquare {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float side = sc.nextFloat();
+        float area = side*side;
+
+        System.out.print("Area of Square =" + area);
+    }
 }

654d6f34ccbf090ba60fdc1ab532187770b28030
sanjaych9517 <sanjaych9517@gmail.com>
2026-02-13 15:17:26 +0530

    Update areaOfCircle.java

--- Day_03/areaOfCircle.java
+++ Day_03/areaOfCircle.java
@@ -1,5 +1,10 @@
 package Day_03;
-
+import java.util.*;
 public class areaOfCircle {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float radius = sc.nextFloat();
+        float area = 3.14f*radius*radius;
+
+        System.out.print("Area of circle =" + area);
 }

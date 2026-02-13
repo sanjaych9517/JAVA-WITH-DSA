0e8f27259b610449925d5fd4eec7553976140d5c
sanjay <167325905+Sanjay181912@users.noreply.github.com>
2026-02-13 18:09:59 +0530

    adding our day 6

---
+++ Day_06/invertedStarPattern.java
@@ -0,0 +1,15 @@
+package Day_06;
+import java.util.Scanner;
+public class invertedStarPattern {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.print("Plz inter number of row : ");
+        int row = sc.nextInt();
+        for( int i = 1 ; i <= row ; i++){
+            for(int j = 1 ; j <= (row - i +1) ; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day_06/printHalfPattern.java
@@ -0,0 +1,16 @@
+package Day_06;
+import java.util.*;
+public class printHalfPattern {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Plz inter number of row : ");
+        int row = sc.nextInt();
+        for( int i = 1 ; i <= row ; i++){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print( "\t" +j);
+            }
+            System.out.println();
+        }
+    }
+}

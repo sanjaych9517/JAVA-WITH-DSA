b6f86834f8c184f12fb90aceea868967ef58492c
sanjay <167325905+Sanjay181912@users.noreply.github.com>
2026-02-19 09:23:44 +0530

    array part 02 upload

---
+++ Day10Array_Part_02/MaxSubarrayPrefix.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class MaxSubarrayPrefix {
+    public static void prefixSum(int numbers []){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+
+        prefix[0] = numbers[0];
+
+        // calculate Prefix Array.
+        for(int i = 1; i < prefix.length; i++){
+            prefix[i] = prefix[i-1] + numbers[i];
+        }
+        for(int i = 0; i < numbers.length; i++){
+            int start =i;
+            for (int j = i; j < numbers.length; j++){
+                 int end = j;
+                 currSum = start ==0 ? prefix[end] : prefix[end] - prefix[start-1];
+                 if(maxSum < currSum){
+                maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Maxsum = " + maxSum);
+
+    }
+   public static void main(String[] args) {
+    int numbers [] = {1, -2, 6, -1, 3};
+    prefixSum(numbers);
+   }
+}
---
+++ Day10Array_Part_02/kadans.java
@@ -0,0 +1,20 @@
+public class kadans {
+   public static void kadans(int numbers []){
+     int maxSum = Integer.MIN_VALUE;
+     int currSum = 0;
+     for(int i =0; i < numbers.length; i++){
+          currSum += numbers[i];
+          if(currSum < 0){
+            currSum =0;
+        }
+            maxSum = Math.max(currSum, maxSum );
+    }
+           System.out.println( "our max subarray sum is : "  + maxSum );
+        }
+ 
+
+    public static void main(String[] args) {
+        int numbers [] = {-2, -3, 4, -1, -2, 1, 5, 3};
+        kadans(numbers);
+    }
+}

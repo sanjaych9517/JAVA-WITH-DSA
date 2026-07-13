d04405dabf474cf23738815560421a9f1aeb5a43
unknown <sanjaych9517@gmail.com>
2026-07-13 22:23:28 +0530

    GetterAndSetter Method

--- Day_15_OOPS/GetterAndSetter.java
+++ Day_15_OOPS/GetterAndSetter.java
@@ -1,5 +1,36 @@
 package Day_15_OOPS;
 
 public class GetterAndSetter {
-    
+    public static void main(String[] args) {
+
+        Pen p1 = new Pen();
+
+        p1.setColor("Red");
+        System.out.println(p1.getColor());
+
+        p1.setTip(6);
+        System.out.println(p1.getTip());
+    }
+}
+
+class Pen {
+
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void setTip(int newTip) {
+        this.tip = newTip;
+    }
 }

2d6be3b5900e2453f5ffbaed03be61202839a375
unknown <sanjaych9517@gmail.com>
2026-07-27 18:42:26 +0530

    solve N netural number sum using Recursion

--- Day_16_recursionBasic_part_01/printSumOfNeturalNum.java
+++ Day_16_recursionBasic_part_01/printSumOfNeturalNum.java
@@ -1,12 +1,12 @@
 package Day_16_recursionBasic_part_01;
 
 public class printSumOfNeturalNum {
-    public static int  calcSum(int n) {
+    public static int calcSum(int n) {
 
-        if (n== 1) {
+        if (n == 1) {
             return 1;
         }
-         int snm1 = calcSum(n-1);
+        int snm1 = calcSum(n - 1);
         int Sn = n + snm1;
         return Sn;
     }

b3b1f08f0729e093c5c41d2e7f2fc3eba91381dd
unknown <sanjaych9517@gmail.com>
2026-07-27 18:39:41 +0530

    change in readme.md file

---
+++ Day_16_recursionBasic_part_01/printSumOfNeturalNum.java
@@ -0,0 +1,18 @@
+package Day_16_recursionBasic_part_01;
+
+public class printSumOfNeturalNum {
+    public static int  calcSum(int n) {
+
+        if (n== 1) {
+            return 1;
+        }
+         int snm1 = calcSum(n-1);
+        int Sn = n + snm1;
+        return Sn;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(calcSum(n));
+    }
+}

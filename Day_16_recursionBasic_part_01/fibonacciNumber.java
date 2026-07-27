eb1fa417111b0308dd3b68a0acebb91e6589a40f
unknown <sanjaych9517@gmail.com>
2026-07-27 21:27:59 +0530

    solve fibonacci Serious

---
+++ Day_16_recursionBasic_part_01/fibonacciNumber.java
@@ -0,0 +1,15 @@
+package Day_16_recursionBasic_part_01;
+
+public class fibonacciNumber {
+     public static int feb(int n){
+        if(n==1 || n==2){
+            return n;
+        }
+        return feb(n-1) + feb(n-2);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(feb(9));
+    }
+}

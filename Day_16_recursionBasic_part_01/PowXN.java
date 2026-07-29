c0dbf81dd69a3d1eb528e230e0de63374e427ff3
unknown <sanjaych9517@gmail.com>
2026-07-29 06:51:24 +0530

    create power function

---
+++ Day_16_recursionBasic_part_01/PowXN.java
@@ -0,0 +1,16 @@
+package Day_16_recursionBasic_part_01;
+  // print the number of Xpow(N);
+public class PowXN {
+      
+  public static int pow(int x, int n){
+       if(n==0){
+        return 1;
+       }
+
+       return x*pow(x, (n-1));
+  }
+    public static void main(String[] args) {
+      System.out.println(pow(2,10));
+    }
+  
+}

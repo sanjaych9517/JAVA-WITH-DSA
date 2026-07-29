cdb184e8654a5c644c58012bd6fce8e1631c1353
unknown <sanjaych9517@gmail.com>
2026-07-29 07:14:00 +0530

    reduce time complexity Power function

---
+++ Day_16_recursionBasic_part_01/reducePowXN.java
@@ -0,0 +1,21 @@
+public class reducePowXN {
+
+    public static int reducePowXN(int a, int n){
+     if(n == 0){
+        return 1;
+     }
+      int halfPower = reducePowXN(a, n/2);
+      int halfPowerSq = halfPower * halfPower;
+
+      // n is odd
+
+      if(n%2 != 0){
+        halfPowerSq = a * halfPowerSq;
+      }
+      return halfPowerSq;
+
+    }
+     public static void main(String[] args) {
+        System.out.println(reducePowXN(2, 4));
+     }
+}

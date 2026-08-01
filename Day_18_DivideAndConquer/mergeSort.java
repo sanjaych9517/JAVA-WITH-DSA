947c9ce3747d2248f8a6d37f7a98c8de5f6c4942
unknown <sanjaych9517@gmail.com>
2026-08-01 17:36:53 +0530

    today we start Divide & conquer day 18

---
+++ Day_18_DivideAndConquer/mergeSort.java
@@ -0,0 +1,14 @@
+package Day_18_DivideAndConquer;
+
+public class mergeSort {
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]+ "");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr [] = {6,3,9,5,2,8};
+    }
+}
